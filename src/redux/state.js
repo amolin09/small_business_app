f0c93d18c72909d8d814e0c7e30357224a8ee32f
amolin09 <alex.molina5829@gmail.com>
2023-09-18 01:18:43 -0500

    completed project

---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+export default {
+	user: '',
+	listings: [
+		{
+			id: 0,
+			name: "Home Slice Pizza",
+			description: "Home Slice Pizza is an independent neighborhood pizza joint serving authentic NY-style pizza - by the pie or by the slice - to nice people like you. We offer our homemade, hand tossed, bond fide pies to either dine in or carry out.",
+			hours: "11AM - 11PM",
+			address: "1215 S Congress Ave, Austin, TX 78704"
+		},
+		{
+			id: 1,
+			name: "Joe's Coffee",
+        description: "Stumptown coffee, backed goods & sandwiches in colorful digs with outdoor area for people-watching.",
+        hours: "7AM - 9PM",
+        address: "242 W 2nd St. Austin, TX 78701"
+		},
+		{
+			id: 2,
+			name: 'Best Buy',
+			description:
+				'At Best Buy, our purpose is to enrich lives through technology',
+			hours: '10AM – 8PM',
+			address: '3201 I-35 N Suite 600, Round Rock, TX 78664',
+		},
+		
+	],
+	map: '',
+};
